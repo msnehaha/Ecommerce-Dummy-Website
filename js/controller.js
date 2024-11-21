5c4356d6813ccca8f47543e59c4fc915ee3e68ba
msnehaha <snehasth11@gmail.com>
2024-11-21 14:25:41 +0545

    Add files via upload

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,30 @@
-// import { API_URL }   from "./config"; 
+'use strict';
 
 
 
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            const { latitude, longitude } = position.coords;
+            console.log(latitude, longitude);
 
-const data = await fetch('https://dummyjson.com/products')
-.then(res => res.json());
+            // Initialize the map centered on the user's location
+            var map = L.map('map').setView([latitude, longitude], 13);
 
-console.log(data);
+            // Add OpenStreetMap tile layer
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            // Add a marker at the user's location
+            L.marker([latitude, longitude]).addTo(map)
+                .bindPopup('You are here!')
+                .openPopup();
+        },
+        function() {
+            alert("Couldn't get your position.");
+        }
+    );
+} else {
+    alert("Geolocation is not supported by this browser.");
+}
