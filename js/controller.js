b92d7dbfc719f952bbad47c1313c8e1e1ae4fece
msnehaha <snehasth11@gmail.com>
2024-11-22 15:04:32 +0545

    Add files via upload

--- js/controller.js
+++ js/controller.js
@@ -1,30 +1,73 @@
 'use strict';
 
+const featuredContainer = document.querySelector('.featured__body');
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                const { latitude, longitude } = position.coords;
+                // Initialize the map centered on the user's location
+                const map = L.map('map').setView([latitude, longitude], 13);
+    
+                // Add OpenStreetMap tile layer
+                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(map);
+    
+                // Add a marker at the user's location
+                L.marker([latitude, longitude]).addTo(map)
+                    .bindPopup(`<div class='popup'>
+                        <h1>Panipokhari</h1>
+
+                        <p>Phone: +123 456 789</p>
+                        
+                        
+                        </div>
+                        `)
+                    .openPopup();
+            },
+            function() {
+                alert("Couldn't get your position.");
+            }
+        );
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
 
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        function(position) {
-            const { latitude, longitude } = position.coords;
-            console.log(latitude, longitude);
 
-            // Initialize the map centered on the user's location
-            var map = L.map('map').setView([latitude, longitude], 13);
+//Fetching Data from API
 
-            // Add OpenStreetMap tile layer
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
 
-            // Add a marker at the user's location
-            L.marker([latitude, longitude]).addTo(map)
-                .bindPopup('You are here!')
-                .openPopup();
-        },
-        function() {
-            alert("Couldn't get your position.");
-        }
-    );
-} else {
-    alert("Geolocation is not supported by this browser.");
+
+const API_URL = 'https://dummyjson.com/products/';
+
+
+console.log(fetch(API_URL).then(res=>res.json()).then(data =>data));
+
+
+const getProduct = function(id)
+{
+    ( fetch(`${API_URL}/${id}`).then(res =>{
+        return res.json();
+        }).then(data => {
+            console.log(data);
+            renderData(data)})) ;
+}
+
+
+
+// getProduct(1);
+
+const renderData = function(data)
+{
+    const html = `<div class="featured__items ">
+                <div class="featured__items--image">
+                    <img src="${data.images}" alt="Orange  " class="image-primary">
+                </div>
+                <h4 class="featured__items--info name">${data.title}</h4>
+                <h6 class="featured__items--price price">$${data.price}</h6>
+            </div>`;
+featuredContainer.insertAdjacentHTML('afterbegin', html);
+        
 }
